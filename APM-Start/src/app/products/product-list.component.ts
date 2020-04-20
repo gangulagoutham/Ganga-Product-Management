c932d2b535dad06a4b85c8faca41657ff4cbd4c0
Goutham Gangula <gouthamgangula@Gouthams-MacBook-Pro.local>
2020-04-19 21:35:00 -0400

    created nested component

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,16 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
 
 @Component({
     selector: 'pm-products',
-    templateUrl: './product-list.component.html'
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
     pageTitle = 'Product List!';
     imageWidth = 50 ;
     imageMargin = 2;
     showImage = false ;
-    listFilter = 'cart';
+    constructor() {
+    this.filteredProducts = this.products;
+    this.listFilter = 'cart';
+    }
+    private _listFilter;
+  public get listFilter() {
+    return this._listFilter;
+  }
+  public set listFilter(value) {
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+  }
+    filteredProducts: IProduct[];
     products: IProduct[] = [
         {
             productId: 2,
@@ -34,7 +47,14 @@ export class ProductListComponent {
           },
     ];
 
+    performFilter(filterBy: string): IProduct[] {
+      return this.products.filter((product: IProduct) =>
+       product.productName.toLocaleLowerCase().indexOf(filterBy.toLocaleLowerCase()) !== -1) ;
+    }
     toggleImage(): void {
      this.showImage = !this.showImage;
+    }
+    ngOnInit(): void {
+      console.log('in ng onit method');
     }
 }
