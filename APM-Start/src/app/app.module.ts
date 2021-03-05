c4dad79b0e51dc10a5d1a705213b8f74fb7a72c7
Goutham Gangula <gouthamgangula@Gouthams-MBP.attlocal.net>
2021-03-04 21:29:47 -0500

    added routing to application

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -6,18 +6,30 @@ import { ProductListComponent } from './products/product-list.component';
 import {HttpClientModule } from '@angular/common/http';
 import { ConvertToSpacePipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ConvertToSpacePipe,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'products' , component: ProductListComponent},
+      {path: 'products/:id' , component: ProductDetailComponent},
+      {path: 'welcome' , component: WelcomeComponent},
+      {path: '' , redirectTo: 'welcome', pathMatch: 'full'},
+      {path: '**' , redirectTo: 'welcome', pathMatch: 'full' }
+    ])
   ],
   bootstrap: [AppComponent]
 })
